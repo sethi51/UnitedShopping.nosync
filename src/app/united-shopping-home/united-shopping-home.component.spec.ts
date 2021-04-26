6f571e9bf802abd129841e9851b1224d3bc50491
Abhishek Sethi <abhisethi.53@gmail.com>
2021-04-26 12:55:49 +0530

    Angular Version upgraded to 11.X

--- src/app/united-shopping-home/united-shopping-home.component.spec.ts
+++ src/app/united-shopping-home/united-shopping-home.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UnitedShoppingHomeComponent } from './united-shopping-home.component';
 
@@ -6,7 +6,7 @@ describe('UnitedShoppingHomeComponent', () => {
   let component: UnitedShoppingHomeComponent;
   let fixture: ComponentFixture<UnitedShoppingHomeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UnitedShoppingHomeComponent ]
     })
